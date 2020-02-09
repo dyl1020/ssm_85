52ae897f0d3470164c268ebfe7d198ec4103798d
lin <2921118529@qq.com>
2020-02-09 20:16:13 +0800

    上传

---
+++ ssm_85/oppo/src/main/java/com/itheima/User.java
@@ -0,0 +1,30 @@
+package com.itheima;
+
+/**
+ * @program: ssm_85
+ * @ClassName User
+ * @description:
+ * @author: dyl
+ * @create: 2020-02-09 16:32
+ **/
+
+public class User {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
