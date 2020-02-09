871429d4e8a1c6f60c7a8f25abb2fab8c5024bdd
lin <2921118529@qq.com>
2020-02-09 20:13:07 +0800

    上传ssm_pari

---
+++ ssm_pari/ssm_dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -0,0 +1,13 @@
+package com.itheima.dao;
+
+import com.itheima.pojo.Items;
+
+import java.util.List;
+
+public interface ItemsDao {
+    //查询所有
+    List<Items> findAll();
+
+    //保存
+    int save(Items items);
+}
---
+++ ssm_pari/ssm_model/src/main/java/com/itheima/pojo/Items.java
@@ -0,0 +1,80 @@
+package com.itheima.pojo;
+
+import java.sql.Date;
+
+/**
+ * @program: ssm_pari
+ * @ClassName Items
+ * @description:
+ * @author: dyl
+ * @create: 2020-02-09 00:27
+ **/
+
+public class Items {
+    private Integer id;
+    private String name;
+    private Float price;
+    private String pic;
+    private Date createtime;
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", pic='" + pic + '\'' +
+                ", createtime=" + createtime +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+}
